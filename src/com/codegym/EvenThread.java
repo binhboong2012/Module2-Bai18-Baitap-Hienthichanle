14500837001d2d6faae2f152348136fd4b649e39
binhboong2012 <binhcauduongbo@gmail.com>
2022-01-27 21:50:39 +0700

    Hien thi so chan le

---
+++ src/com/codegym/EvenThread.java
@@ -0,0 +1,17 @@
+package com.codegym;
+
+public class EvenThread extends Thread {
+
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i += 2) {
+            System.out.println("Chẵn = " + i);
+            try {
+                Thread.sleep(15);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/com/codegym/TestThread.java
@@ -0,0 +1,16 @@
+package com.codegym;
+
+public class TestThread {
+
+    public static void main(String[] args) {
+	EvenThread evenThread = new EvenThread();
+    OddThread oddThread = new OddThread();
+    oddThread.start();
+        try {
+            oddThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        evenThread.start();
+    }
+}
